fa7a82f7eb538b1bc12cc5661daa723944cca6af
Michael Weir <michaelweir@Michaels-MacBook-Pro-2.local>
2025-12-07 20:01:53 -0500

    api.py flake8 fixes

--- api.py
+++ api.py
@@ -3,7 +3,6 @@ import base64
 import matplotlib.pyplot as plt
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 
 from physics import quasi_steady_flap
@@ -36,12 +35,23 @@ def simulate_plot(req: SimRequest):
     # Create 6 subplot figure like MATLAB version
     fig, axs = plt.subplots(3, 2, figsize=(10, 10))
 
-    axs[0, 0].plot(r["t"], r["L"]); axs[0, 0].set_title("Lift vs Time")
-    axs[0, 1].plot(r["t"], r["D"]); axs[0, 1].set_title("Drag vs Time")
-    axs[1, 0].plot(r["t"], r["P"]); axs[1, 0].set_title("Power vs Time")
-    axs[1, 1].plot(r["t"], r["eta"]); axs[1, 1].set_title("Efficiency")
-    axs[2, 0].plot(r["t"], r["theta_deg"]); axs[2, 0].set_title("Pitch Angle")
-    axs[2, 1].plot(r["t"], r["x_pos"]); axs[2, 1].set_title("Stroke Position")
+    axs[0, 0].plot(r["t"], r["L"])
+    axs[0, 0].set_title("Lift vs Time")
+
+    axs[0, 1].plot(r["t"], r["D"])
+    axs[0, 1].set_title("Drag vs Time")
+
+    axs[1, 0].plot(r["t"], r["P"])
+    axs[1, 0].set_title("Power vs Time")
+
+    axs[1, 1].plot(r["t"], r["eta"])
+    axs[1, 1].set_title("Efficiency")
+
+    axs[2, 0].plot(r["t"], r["theta_deg"])
+    axs[2, 0].set_title("Pitch Angle")
+
+    axs[2, 1].plot(r["t"], r["x_pos"])
+    axs[2, 1].set_title("Stroke Position")
 
     for ax in axs.flat:
         ax.set_xlabel("Time (s)")
